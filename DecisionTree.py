0465444a19858a8bf98d1f2e2722dd1f4a6cc219
JJJJJJpl <jedrek.stf@interia.pl>
2024-05-16 20:19:10 +0200

    gotowe drzewo

--- DecisionTree.py
+++ DecisionTree.py
@@ -35,6 +35,7 @@ def SSR(Data,a,b):
 
 
 def Make_node(Data_Arr, Index):
+    #print(Index," - ",Data_Arr,"\n")
 
     #warunek stopu
     if len(Data_Arr) <= Minimum_Elems or (Index*2)+1 > Depth_Max:
@@ -62,13 +63,13 @@ def Make_node(Data_Arr, Index):
 
             for b in range(0,len(Data_Arr)-1):
 
-                if Data_Arr[b][LookingFor] != Data_Arr[b+1][LookingFor]:
+                if Data_Arr[b][a] != Data_Arr[b+1][a]:
 
-                    s = SSR(Data_Arr, a, (Data_Arr[b][LookingFor] + Data_Arr[b+1][LookingFor])/2 )
+                    s = SSR(Data_Arr, a, (Data_Arr[b][a] + Data_Arr[b+1][a])/2 )
                     if s < S:
                         S = s
                         A = a
-                        B = (Data_Arr[b][LookingFor] + Data_Arr[b+1][LookingFor])/2
+                        B = (Data_Arr[b][a] + Data_Arr[b+1][a])/2
                     
                     pass
     
@@ -90,24 +91,47 @@ def Make_node(Data_Arr, Index):
 
 
 
+def show_tree():
 
+
+    l = 30
+    r = 1
+    pos = 2
+    print((l-3)*" ",TREE[1])
+    for i in range(4):
+        l = int(l*0.6)
+        v = l*" "
+        r = r*2
+        for j in range(r):
+            v += str(TREE[pos])
+            t = l-int(len(str(TREE[pos]))/2)
+            if t > 0: v += t * " "
+            pos += 1
+        print(v)
+        
+
+    pass
 
 
 
 
 if __name__ == "__main__":
+
     ARR = [
         [3,1,3],
         [0,2,5],
         [3,2,8],
         [1,1,1],
+        [1,2,6],
         [4,1,4],
         [3,1,3],
         [1,2,6],
-        [4,2,9],
         [2,2,7]
     ]
     Make_node(ARR,1)
     print(TREE)
+
+    show_tree()
+    
 
 
